03a7a3096dcea5a3ac28ceb31ef3ee4e9c6bffc4
Jay Sm <jaysmailg14@gmail.com>
2023-10-06 17:55:47 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view></router-view>
+
+  <router-link class="text-blue-700 underline inline-block pt-5" :to="{ name: 'home' }">Home View</router-link>
+  <br>
+  <router-link class="text-blue-700 underline inline-block" :to="{ name: 'about' }">About View</router-link>
+</template>
+
+<script setup>
+</script>
+
+<style></style>
